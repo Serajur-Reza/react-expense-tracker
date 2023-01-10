8748206ae5bf9431f08c5afdaa045d8ae4e8b63e
Serajur-Reza <rezainfinity54@gmail.com>
2023-01-10 19:48:06 +0600

    pwa-configured-done

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from "vite-plugin-pwa"
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { VitePWA } from "vite-plugin-pwa"
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // plugins: [
+//   //   VitePWA({
+//   //     registerType: 'autoUpdate',
+//   //     workbox: {
+//   //       globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+//   //     }
+//   //   })
+//   // ]
+// })
+
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-  plugins: [react()],
-  // plugins: [
-  //   VitePWA({
-  //     registerType: 'autoUpdate',
-  //     workbox: {
-  //       globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-  //     }
-  //   })
-  // ]
+  plugins: [
+    VitePWA({
+      injectRegister: 'auto'
+    })
+  ]
 })
+
 
 // import { defineConfig } from 'vite'
 // import { VitePWA } from 'vite-plugin-pwa'
